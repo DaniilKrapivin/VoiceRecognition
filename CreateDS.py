61cca6c0d3495c155d3b6278b9bb89c0c066ca2c
DaniilKrapivin <105483245+DaniilKrapivin@users.noreply.github.com>
2022-05-13 16:29:30 +0800

    Update CreateDS.py

--- CreateDS.py
+++ CreateDS.py
@@ -30,7 +30,6 @@ class SoundDataset(Dataset):
             self.labels.append(csvData.iloc[i, li])
 
     def __getitem__(self, index):
-        # format the file path and load the file
         path = os.path.join(self.file_path, self.file_names[index])
         soundData, sample_rate = torchaudio.load(path,normalize=True)
 
@@ -38,15 +37,12 @@ class SoundDataset(Dataset):
             resample_transform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=self.resample)
             soundData = resample_transform(soundData)
 
-        # This will convert audio files with two channels into one
         soundData = torch.mean(soundData, dim=0, keepdim=True)
-
-        # Convert audio to log-scale Mel spectrogram
+        
         melspectrogram_transform = torchaudio.transforms.MelSpectrogram(sample_rate=self.resample, n_mels=self.n_mels)
         melspectrogram = melspectrogram_transform(soundData)
         melspectogram_db = torchaudio.transforms.AmplitudeToDB()(melspectrogram)
 
-        # Make sure all spectrograms are the same size
         fixed_length = 3 * (self.resample // 200)
         if melspectogram_db.shape[2] < fixed_length:
             melspectogram_db = torch.nn.functional.pad(melspectogram_db, (0, fixed_length - melspectogram_db.shape[2]))
